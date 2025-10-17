de1b482ebf20c990695d51be424f025f02b9bf40
Harrison Spurle <harryspurle@gmail.com>
2025-10-17 10:26:13 +0100

    Slow problem 217

---
+++ 217.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        seen = dict()
+        for item in nums:
+            if item in seen:
+                seen[item] += 1
+            else:
+                seen[item] = 1
+        if seen[max(seen, key=seen.get)] >=2:
+            return True
+        return False
+
+sol = Solution()
+print(sol.containsDuplicate([1,2,3,1]))
