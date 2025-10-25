fbaa019a6ed0fb29ede103927a3655fdab6734be
Harrison Spurle <harryspurle@gmail.com>
2025-10-25 20:04:12 +0100

    Problem 1929

--- 1920.py
+++ 1920.py
@@ -1,10 +1,6 @@
 class Solution(object):
     def buildArray(self, nums):
         output = []
-        #for progression in nums:
         for index in range(len(nums)):
             output.append(nums[nums[index]])
         return output
-
-sol = Solution()
-print(sol.buildArray([0,2,1,5,3,4]))
