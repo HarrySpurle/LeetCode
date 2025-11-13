f36d1b95019f5edd3ea7622d8103a104cca44fac
Harrison Spurle <harryspurle@gmail.com>
2025-11-13 17:27:40 +0000

    Problem 70

--- 2894.py
+++ 2894.py
@@ -1,7 +1,3 @@
 class Solution:
     def differenceOfSums(self, n, m):
         return sum(i for i in range(0, n+1) if i%m!=0) - sum(i for i in range(0, n+1) if i%m==0)
-    
-sol = Solution()
-print(sol.differenceOfSums(10,3))
-        
---
+++ 70.py
@@ -0,0 +1,7 @@
+class Solution:
+    def climbStairs(self, n):
+        distinct = [1,1,2,3]
+        while distinct.index(distinct[-1]) <= n:
+            distinct.append(distinct[-1] + distinct[-2])
+        return distinct[n]
+        
